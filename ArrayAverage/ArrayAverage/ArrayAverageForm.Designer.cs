6796075b5ead409a614569b513086157d725458a
Joelle Ishimwe <36892599+joelleishimwe@users.noreply.github.com>
2020-05-26 09:57:35 -0400

    new

---
+++ ArrayAverage/ArrayAverage/ArrayAverageForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ArrayAverage
+{
+    partial class frmArrayAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstArray = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.mniNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstArray
+            // 
+            this.lstArray.FormattingEnabled = true;
+            this.lstArray.Location = new System.Drawing.Point(178, 127);
+            this.lstArray.Name = "lstArray";
+            this.lstArray.Size = new System.Drawing.Size(445, 212);
+            this.lstArray.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(244, 139);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(350, 182);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Font = new System.Drawing.Font("Sitka Subheading", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAverage.Location = new System.Drawing.Point(369, 360);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(95, 36);
+            this.btnAverage.TabIndex = 2;
+            this.btnAverage.Text = "Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit,
+            this.mniNew});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(180, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(262, 88);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(68, 24);
+            this.lblAverage.TabIndex = 4;
+            this.lblAverage.Text = "label1";
+            // 
+            // mniNew
+            // 
+            this.mniNew.Name = "mniNew";
+            this.mniNew.Size = new System.Drawing.Size(180, 22);
+            this.mniNew.Text = "New";
+            this.mniNew.Click += new System.EventHandler(this.mniNew_Click);
+            // 
+            // frmArrayAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstArray);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "frmArrayAverage";
+            this.Text = "Array Average                   by: Joelle Ishimwe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstArray;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.ToolStripMenuItem mniNew;
+    }
+}
+
---
+++ ArrayAverage/ArrayAverage/ArrayAverageForm.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * Created by: Joelle Ishimwe
+ * Created on: 24-May-2020
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #36 - Introduction to Arrays
+ * This program creates an array of size 10. When the user 
+ * clicks on the "Start" button. It generates 10 random numbers, 
+ * each between 1 and 10 and for each random number, it displays 
+ * it in a list box. It also  assigns it to the subsequent cell in 
+ * the array. When the user clicks on the "Calculate Average" button, 
+ * it calculates the average of the sum of the 10 numbers in the array.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayAverage
+{
+    public partial class frmArrayAverage : Form
+    {
+        //declare global variables and constants
+        const int MAX_ARRAY_SIZE = 10;
+        int[] arrayOfNumbers = new int[MAX_ARRAY_SIZE];
+
+        public frmArrayAverage()
+        {
+            InitializeComponent();
+
+            //hide items
+            this.lblAverage.Hide();
+            this.lstArray.Hide();
+            this.btnAverage.Hide();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //have the button disapear
+            this.btnStart.Hide();
+
+            //Show items
+            this.lstArray.Show();
+            this.btnAverage.Show();
+
+            //declare variables and constants
+            int randomNumber;
+            int counter;
+            Random randomNumberGenerator = new Random();
+
+            //clear listbox
+            this.lstArray.Items.Clear();
+
+            //a for loop that displays 10 random numbers in a listbox
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter ++)
+            {
+                //generate a new random number between 1 and the max array size
+                randomNumber = randomNumberGenerator.Next(1, MAX_ARRAY_SIZE + 1);
+
+                //assign randomNumber to the cell at position "counter" in the array
+                arrayOfNumbers[counter] = randomNumber;
+
+                //add the random number to the listbox
+                lstArray.Items.Add(randomNumber); 
+
+                //REFRESH
+                this.Refresh();
+            }
+
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            //close the program
+            this.Close();
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            //declare variables and constants
+            double average = 0;
+            double sum = 0;
+            int counter;
+
+            //calculate the sum of all the values in the array
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                sum = sum + arrayOfNumbers[counter];
+            }
+
+            //calculate the average of all the values in the array
+            average = sum / arrayOfNumbers.Length;
+
+            //display answer in label
+            this.lblAverage.Text = "The average is:  " + average;
+            this.lblAverage.Show();
+        }
+
+        private void mniNew_Click(object sender, EventArgs e)
+        {
+            //restart
+            Application.Restart();
+        }
+    }
+}
