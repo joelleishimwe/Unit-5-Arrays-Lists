f9748b4c543a4886c9916d99ea1a305385d2fa2b
Joelle Ishimwe <36892599+joelleishimwe@users.noreply.github.com>
2020-06-04 15:44:49 -0400

    new

--- FindMinValue/FindMinValue/FindMinValueForm.Designer.cs
+++ FindMinValue/FindMinValue/FindMinValueForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace FindMaxValue
+﻿namespace FindMinValue
 {
-    partial class frmFindMaxValue
+    partial class frmFindMinValue
     {
         /// <summary>
         /// Required designer variable.
@@ -28,50 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lstArrayOfValues = new System.Windows.Forms.ListBox();
-            this.lblMaxValue = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
+            this.lblMinValue = new System.Windows.Forms.Label();
+            this.lstArrayOfValues = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
-            // 
-            // lstArrayOfValues
-            // 
-            this.lstArrayOfValues.FormattingEnabled = true;
-            this.lstArrayOfValues.Location = new System.Drawing.Point(187, 142);
-            this.lstArrayOfValues.Name = "lstArrayOfValues";
-            this.lstArrayOfValues.Size = new System.Drawing.Size(402, 173);
-            this.lstArrayOfValues.TabIndex = 0;
-            // 
-            // lblMaxValue
-            // 
-            this.lblMaxValue.AutoSize = true;
-            this.lblMaxValue.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMaxValue.Location = new System.Drawing.Point(264, 357);
-            this.lblMaxValue.Name = "lblMaxValue";
-            this.lblMaxValue.Size = new System.Drawing.Size(51, 18);
-            this.lblMaxValue.TabIndex = 1;
-            this.lblMaxValue.Text = "label1";
             // 
             // btnStart
             // 
             this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStart.Location = new System.Drawing.Point(343, 81);
+            this.btnStart.Location = new System.Drawing.Point(328, 68);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(87, 42);
-            this.btnStart.TabIndex = 2;
+            this.btnStart.TabIndex = 3;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
-            // frmFindMaxValue
+            // lblMinValue
+            // 
+            this.lblMinValue.AutoSize = true;
+            this.lblMinValue.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinValue.Location = new System.Drawing.Point(242, 369);
+            this.lblMinValue.Name = "lblMinValue";
+            this.lblMinValue.Size = new System.Drawing.Size(51, 18);
+            this.lblMinValue.TabIndex = 5;
+            this.lblMinValue.Text = "label1";
+            // 
+            // lstArrayOfValues
+            // 
+            this.lstArrayOfValues.FormattingEnabled = true;
+            this.lstArrayOfValues.Location = new System.Drawing.Point(177, 155);
+            this.lstArrayOfValues.Name = "lstArrayOfValues";
+            this.lstArrayOfValues.Size = new System.Drawing.Size(402, 173);
+            this.lstArrayOfValues.TabIndex = 4;
+            // 
+            // frmFindMinValue
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.lblMaxValue);
+            this.Controls.Add(this.lblMinValue);
             this.Controls.Add(this.lstArrayOfValues);
-            this.Name = "frmFindMaxValue";
-            this.Text = "Find Maximum Value           by:  Joelle Ishimwe";
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmFindMinValue";
+            this.Text = "Find Minimum Value     by:  Joelle Ishimwe";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,9 +79,9 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox lstArrayOfValues;
-        private System.Windows.Forms.Label lblMaxValue;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblMinValue;
+        private System.Windows.Forms.ListBox lstArrayOfValues;
     }
 }
 
---
+++ FindMinValue/FindMinValue/FindMinValueForm.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Created by: Joelle Ishimwe
+ * Created on: 3-June-2020
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #38 - Find Min Value
+ * This program that has an array size of 10 cells. It 
+ * populates the array with 10 random numbers between 
+ * 0 - 500. Then a function is called to traverse the 
+ * array and find the min value. The min value is then 
+ * displayed in a label 
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindMinValue
+{
+    public partial class frmFindMinValue : Form
+    {
+        public frmFindMinValue()
+        {
+            InitializeComponent();
+
+            //hide the label with the min value
+            this.lblMinValue.Hide();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare variables and constants
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_NUMBER = 500;
+            const int MIN_NUMBER = 0;
+            int[] arrayOfNumber = new int[MAX_ARRAY_SIZE];
+            int randomNumber;
+            int counter;
+            int minValue;
+
+            //create a generator that will choose a random number
+            Random RandomGenerator = new Random();
+
+            //clear the listbox
+            lstArrayOfValues.Items.Clear();
+
+            //loop that will generate random numbers and add the array to the listbox 
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                randomNumber = RandomGenerator.Next(MIN_NUMBER, MAX_NUMBER + 1);
+                lstArrayOfValues.Items.Add(randomNumber);
+                arrayOfNumber[counter] = randomNumber;
+            }
+
+            //get the min value
+            minValue = GetMinValue(arrayOfNumber);
+
+            //display the answer in a label
+            this.lblMinValue.Show();
+            lblMinValue.Text = "The minimum value is:  " + minValue;
+        }
+
+        private int GetMinValue(int[] tempArrayOfNumbers)
+        {
+            //declare variables
+            int tempMinValue = 501;
+            int tempCounter;
+
+            //loop that will associate the array lenth to the counter to find the min value
+            for (tempCounter = 0; tempCounter < tempArrayOfNumbers.Length; tempCounter++)
+            {
+                if (tempArrayOfNumbers[tempCounter] < tempMinValue)
+                {
+                    tempMinValue = tempArrayOfNumbers[tempCounter];
+                }
+            }
+
+            //return the value
+            return tempMinValue;
+        }
+    }
+}
