7570ceffd10c59daa048fdadae89bbd062c28475
Joelle Ishimwe <36892599+joelleishimwe@users.noreply.github.com>
2020-06-04 15:23:02 -0400

    new

---
+++ FindMaxValue/FindMaxValue/FindMaxValueForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FindMaxValue
+{
+    partial class frmFindMaxValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstArrayOfValues = new System.Windows.Forms.ListBox();
+            this.lblMaxValue = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstArrayOfValues
+            // 
+            this.lstArrayOfValues.FormattingEnabled = true;
+            this.lstArrayOfValues.Location = new System.Drawing.Point(187, 142);
+            this.lstArrayOfValues.Name = "lstArrayOfValues";
+            this.lstArrayOfValues.Size = new System.Drawing.Size(402, 173);
+            this.lstArrayOfValues.TabIndex = 0;
+            // 
+            // lblMaxValue
+            // 
+            this.lblMaxValue.AutoSize = true;
+            this.lblMaxValue.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaxValue.Location = new System.Drawing.Point(264, 357);
+            this.lblMaxValue.Name = "lblMaxValue";
+            this.lblMaxValue.Size = new System.Drawing.Size(51, 18);
+            this.lblMaxValue.TabIndex = 1;
+            this.lblMaxValue.Text = "label1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(343, 81);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(87, 42);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // frmFindMaxValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblMaxValue);
+            this.Controls.Add(this.lstArrayOfValues);
+            this.Name = "frmFindMaxValue";
+            this.Text = "Find Max Value           by:  Joelle Ishimwe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstArrayOfValues;
+        private System.Windows.Forms.Label lblMaxValue;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ FindMaxValue/FindMaxValue/FindMaxValueForm.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Created by: Joelle Ishimwe
+ * Created on: 1-June-2020
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #37 - Find Max Value
+ * This program that has an array size of 10 cells. It 
+ * populates the array with 10 random numbers between 
+ * 0 - 500. Then a function is called to traverse the 
+ * array and find the max value. The max value is then 
+ * displayed in a label 
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindMaxValue
+{
+    public partial class frmFindMaxValue : System.Windows.Forms.Form
+    {
+        public frmFindMaxValue()
+        {
+            InitializeComponent();
+            
+            //hide the label with the max value
+            this.lblMaxValue.Hide();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare variables and constants
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_NUMBER = 500;
+            const int MIN_NUMBER = 0;
+            int[] arrayOfNumber = new int[MAX_ARRAY_SIZE];
+            int randomNumber;
+            int counter;
+            int maxValue;
+
+            //create a generator that will choose a random number
+            Random RandomGenerator = new Random();
+
+            //clear the listbox
+            lstArrayOfValues.Items.Clear();
+
+            //loop that will generate random numbers and add the array to the listbox 
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                randomNumber = RandomGenerator.Next(MIN_NUMBER, MAX_NUMBER + 1);
+                lstArrayOfValues.Items.Add(randomNumber);
+                arrayOfNumber[counter] = randomNumber;
+            }
+
+            //get the max value
+            maxValue = GetMaxValue(arrayOfNumber);
+
+            //display the answer in a label
+            this.lblMaxValue.Show();
+            lblMaxValue.Text = "The maximum value is:  " + maxValue;
+        }
+
+        private int GetMaxValue(int[] tempArrayOfNumbers)
+        {
+            //declare variables
+            int tempMaxValue = -1;
+            int tempCounter;
+
+            //loop that will associate the array lenth to the counter to find the max value
+            for (tempCounter = 0; tempCounter < tempArrayOfNumbers.Length; tempCounter++)
+            {
+                if(tempArrayOfNumbers[tempCounter] > tempMaxValue)
+                {
+                    tempMaxValue = tempArrayOfNumbers[tempCounter];
+                }
+            }
+            
+            //return the value
+            return tempMaxValue;
+        }
+    }
+}
